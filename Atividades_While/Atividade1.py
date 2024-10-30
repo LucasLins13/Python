1809e1adb443af8815ddf698ac732ecc0446d022
LucasLins13 <lucasglins13@gmail.com>
2024-10-30 10:26:17 -0300

    feat: Atividade1.py

---
+++ Atividades_While/Atividade1.py
@@ -0,0 +1,16 @@
+import os 
+os.system('cls')
+
+
+lista = [1,2,3,4,5,6,7,8,9,10] 
+
+    
+del lista[2] 
+lista.remove(5)  
+lista[0] = 0
+lista.insert(10, 12)
+lista.insert(9, 11)
+print(lista) 
+
+print(len(lista))
+
